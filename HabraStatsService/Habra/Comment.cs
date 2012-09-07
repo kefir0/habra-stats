88df13a9f92e6707aedf1876928a8fda45a84de6
nAXAH1@gmail.com <nAXAH1@gmail.com@4f192250-6efc-a964-58ba-e86f36e11076>
2012-09-07 18:59:52 +0000

    Initial checkin

---
+++ HabraStatsService/Habra/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace HabraStatsService.Habra
+{
+    internal class Comment
+    {
+        public int Id { get; set; }
+        public int Score { get; set; }
+        public int ScorePlus { get; set; }
+        public int ScoreMinus { get; set; }
+        public string Text { get; set; }
+    }
+}
