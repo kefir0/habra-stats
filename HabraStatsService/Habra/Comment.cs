5404c790a5ee81733f59698d92315381eab2a289
nAXAH1@gmail.com <nAXAH1@gmail.com@4f192250-6efc-a964-58ba-e86f36e11076>
2012-09-07 19:26:43 +0000

    First working test

--- HabraStatsService/Habra/Comment.cs
+++ HabraStatsService/Habra/Comment.cs
@@ -1,11 +1,17 @@
 ﻿namespace HabraStatsService.Habra
 {
-    internal class Comment
+    public class Comment
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public int Score { get; set; }
         public int ScorePlus { get; set; }
         public int ScoreMinus { get; set; }
         public string Text { get; set; }
+        public string Url { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("[{0}] {1}", Score, Text);
+        }
     }
 }
---
+++ HabraStatsServiceTest/HabrTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using HabraStatsService.Habra;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HabraStatsServiceTest
+{
+    [TestClass]
+    public class HabrTests
+    {
+        [TestMethod]
+        public void TestGetPosts()
+        {
+            var h = new Habr();
+            var posts = h.GetRecentPosts(10).ToArray();
+            Assert.AreEqual(posts.Length, 10);
+        }
+    }
+}
