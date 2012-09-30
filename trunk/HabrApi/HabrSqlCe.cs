b750f809ee72a18182800b7c14879d47fb909f2e
nAXAH1@gmail.com <nAXAH1@gmail.com@4f192250-6efc-a964-58ba-e86f36e11076>
2012-09-30 20:26:43 +0000

    SQlite removed, SQL Compact added

---
+++ trunk/HabrApi/HabrSqlCe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HabrApi.EntityModel;
+
+namespace HabrApi
+{
+    class HabrSqlCe
+    {
+        private Habr _habr;
+
+        public HabrSqlCe()
+        {
+            _habr = new Habr();
+        }
+
+        public void InsertPost(Post post)
+        {
+            using (var ctx = new HabraStatsEntities())
+            {
+                //ctx.Posts.Add(new Post(){Id=post.Id, Comments = post.Comments, Date = post.Date})
+            }
+        }
+    }
+}
