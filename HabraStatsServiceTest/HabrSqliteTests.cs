6ace3d1dfc47cd306c7ba4818bda73eb1b734e94
nAXAH1@gmail.com <nAXAH1@gmail.com@4f192250-6efc-a964-58ba-e86f36e11076>
2012-09-30 18:53:13 +0000

    SqlLite added

---
+++ HabraStatsServiceTest/HabrSqliteTests.cs
@@ -0,0 +1,16 @@
+﻿using HabrApi;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HabrApiTests
+{
+    [TestClass]
+    public class HabrSqliteTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            var h = new HabrSqlite();
+            h.LoadIntoSqlite(1);
+        }
+    }
+}
