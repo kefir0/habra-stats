6a54d51bc947fa6d02b6aca7cbcab9c8f937b589
nAXAH1@gmail.com <nAXAH1@gmail.com@4f192250-6efc-a964-58ba-e86f36e11076>
2012-10-06 09:28:40 +0000

    Switched to SQL express (SQLCE is too limited)

--- HabrApi/HabrSqlCe.cs
+++ HabrApi/HabrSqlCe.cs
@@ -5,15 +5,15 @@ namespace HabrApi
 {
     public class HabrSqlCe
     {
+        //public 
+
         public void UpsertPost(Post post)
         {
             using (var ctx = HabraStatsEntities.CreateInstance())
             {
-                //ctx.ExecuteStoreCommand("DELETE FROM COMMENTS WHERE PostId=" + post.Id);
-
                 foreach (var comment in post.Comments)
                 {
-                    ctx.Comments.AddObject(comment);
+                    UpsertComment(ctx, comment);
                 }
                 ctx.SaveChanges();
             }
@@ -27,16 +27,16 @@ namespace HabrApi
             }
         }
 
-        //private static void UpsertComment(HabraStatsEntities ctx, Comment comment)
-        //{
-        //    if (ctx.Comments.SingleOrDefault(c => c.Id == comment.Id) != null)
-        //    {
-        //        ctx.Comments.ApplyCurrentValues(comment);
-        //    }
-        //    else
-        //    {
-        //        ctx.Comments.AddObject(comment);
-        //    }
-        //}
+        private static void UpsertComment(HabraStatsEntities ctx, Comment comment)
+        {
+            if (ctx.Comments.SingleOrDefault(c => c.Id == comment.Id) != null)
+            {
+                ctx.Comments.ApplyCurrentValues(comment);
+            }
+            else
+            {
+                ctx.Comments.AddObject(comment);
+            }
+        }
     }
 }
